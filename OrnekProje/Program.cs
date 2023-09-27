4e4601a7588e11fca400ef937874ec414f0c4112
Sedat MENGÜ <sedatmengu@gmail.com>
2023-09-27 23:30:10 +0300

    .

--- OrnekProje/Program.cs
+++ OrnekProje/Program.cs
@@ -1,4 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
 using System.Reflection.Metadata;
 
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine(typeof(char).IsPrimitive);
+// Console.WriteLine(typeof(sbyte).IsPrimitive);
+// Console.WriteLine(typeof(byte).IsPrimitive);
+// Console.WriteLine(typeof(short).IsPrimitive);
+// Console.WriteLine(typeof(ushort).IsPrimitive);
+// Console.WriteLine(typeof(int).IsPrimitive);
+// Console.WriteLine(typeof(uint).IsPrimitive);
+// Console.WriteLine(typeof(long).IsPrimitive);
+// Console.WriteLine(typeof(ulong).IsPrimitive);
+// Console.WriteLine(typeof(float).IsPrimitive);
+// Console.WriteLine(typeof(double).IsPrimitive);
+// Console.WriteLine(typeof(decimal).IsPrimitive);
+
+
+// string @static = "slm";
+
+// Console.WriteLine(@static);
+
+// int a = 0;
+
+// a = 9;
+
+// a=100;
+
+// Console.WriteLine(a);
+
+
+// char @char = 'a';
+
+// Console.WriteLine(@char);
+
+// bool evliMisin = true;
+// bool satistaMi = false;
+
+// Console.WriteLine(evliMisin);
+// Console.WriteLine(satistaMi);
+
+// float @float = 3.14f;
+// float @float2= 3.15F;
+
+// Console.WriteLine(@float);
+// Console.WriteLine(@float2);
+
+
+// double @double = 7.10d;
+// double @double2 = 7.17D;
+
+// Console.WriteLine((@double , @double2));
+
+// decimal @decimal = 12.9m;
+// decimal @decimal2 = 3.2M;
+
+// // Console.WriteLine((@decimal , @decimal2));
+
+// (int @int , 
+// string @string , 
+// char @char) @tuple = (3,"ahmet",'k');
+
+// Console.WriteLine(@tuple);
+
+// Console.WriteLine(@tuple.@char);
+
+// double sayi = 1_000_00000_000;
+
